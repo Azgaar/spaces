80c107b04c39c169708ceffee7b6df7642c1d530
Azgaar <maxganiev@yandex.ru>
2021-03-23 13:41:49 +0300

    idle time to 2 hours

--- server/src/config/session.ts
+++ server/src/config/session.ts
@@ -5,7 +5,7 @@ export const getSessionConfig = () => {
   const saveUninitialized: boolean = false;
   const secret: string = process.env.SESSION_SECRET || "21F463B8C3489";
 
-  const maxAge: number = Number(process.env.SESSION_LIFETIME) || 1000 * 60 * 60 * 8;
+  const maxAge: number = Number(process.env.SESSION_LIFETIME) || 1000 * 60 * 60 * 2; // two hours
   const sameSite: boolean = true;
   const httpOnly: boolean = true;
   const secure: boolean = process.env.NODE_ENV === "production";
