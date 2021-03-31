729c95044cbe5a0942f78c378cf703acba9da9e7
Azgaar <maxganiev@yandex.ru>
2021-03-31 21:30:17 +0300

    need to send data for state update

--- server/src/controllers/updateUser.controller.ts
+++ server/src/controllers/updateUser.controller.ts
@@ -17,9 +17,9 @@ export const updateUserController = catchAsync(async (req, res, next) => {
   const {email, firstName, lastName, password} = req.body;
 
   const correctPassword = await compare(password, user.password);
-  if (!correctPassword) return next(new ApiError(httpStatus.UNAUTHORIZED, `Password ${password} is not correct for user ${user.email}`));
+  if (!correctPassword) return next(new ApiError(httpStatus.UNAUTHORIZED, `Password ${password} is not correct for user ${email}`));
 
   const userData = {email, firstName, lastName, password, role: user.role};
   await updateUser(user, userData);
-  res.status(httpStatus.NO_CONTENT).end();
+  res.status(httpStatus.NO_CONTENT).send({email, firstName, lastName});
 });
