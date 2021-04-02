7a7060ba5ebbd8d764da1be81494663c1e6abbc9
Azgaar <maxganiev@yandex.ru>
2021-04-02 17:02:09 +0300

    futher PrivateRoute enhancement - access roles

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,7 +2,7 @@ import React, {Suspense} from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Spinner from "./Spinner/Spinner";
-import PrivateRoute from "../pages/PrivateRoute";
+import {PrivateRoute, AccessRole} from "../pages/PrivateRoute";
 
 const Signin = React.lazy(() => import("../pages/Signin/Signin"));
 const Signup = React.lazy(() => import("../pages/Signup/Signup"));
@@ -20,15 +20,15 @@ function App() {
       <Suspense fallback={<Spinner />}>
         <Switch>
           <Route exact path="/"><Redirect to="/dashboard" /></Route>
-          <Route path="/signin" render={() => <Signin />} />
-          <Route path="/signup" render={() => <Signup />} />
-          <Route path="/forgotPassword" render={() => <ForgotPassword />} />
-          <PrivateRoute path="/logout" component={Logout} />
-          <PrivateRoute path="/profile" component={Profile} />
-          <PrivateRoute path="/editProfile" component={ProfileEdit} />
-          <PrivateRoute path="/changePassword" component={PasswordChange} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/users" adminOnly component={Users} />
+          <PrivateRoute path="/signin" access={AccessRole.UNLOGGED} component={Signin} />
+          <PrivateRoute path="/signup" access={AccessRole.UNLOGGED} component={Signup} />
+          <PrivateRoute path="/logout" access={AccessRole.LOGGED} component={Logout} />
+          <PrivateRoute path="/profile" access={AccessRole.LOGGED} component={Profile} />
+          <PrivateRoute path="/editProfile" access={AccessRole.LOGGED} component={ProfileEdit} />
+          <PrivateRoute path="/changePassword" access={AccessRole.LOGGED} component={PasswordChange} />
+          <PrivateRoute path="/forgotPassword" access={AccessRole.LOGGED} component={ForgotPassword} />
+          <PrivateRoute path="/dashboard" access={AccessRole.LOGGED} component={Dashboard} />
+          <PrivateRoute path="/users" access={AccessRole.ADMIN} component={Users} />
           <Route path="/*"><Redirect to="/dashboard" /></Route>
         </Switch>
       </Suspense>
--- client/src/pages/PrivateRoute.tsx
+++ client/src/pages/PrivateRoute.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import {Route, Redirect} from "react-router-dom";
 import {useUser} from "../hooks";
 
-const PrivateRoute = (props: {path: string; adminOnly?: boolean, component: React.FC}) => {
-  const {isAuthenticated, isAdmin} = useUser();
-  console.log(props, useUser());
+enum AccessRole {
+  UNLOGGED = "unlogged",
+  LOGGED = "logged",
+  ADMIN = "admin"
+};
 
-  if (!isAuthenticated) return <Redirect to="/signin" />;
-  if (props.adminOnly && !isAdmin) return <Redirect to="/dashboard" />;
+const PrivateRoute = (props: {path: string; access: AccessRole, component: React.FC}) => {
+  const {isAuthenticated, isAdmin} = useUser();
+
+  if (props.access === AccessRole.UNLOGGED && isAuthenticated) return <Redirect to="/dashboard" />;
+  if (props.access === AccessRole.LOGGED && !isAuthenticated) return <Redirect to="/signin" />;
+  if (props.access === AccessRole.ADMIN && !isAdmin) return <Redirect to="/dashboard" />;
   return <Route path={props.path} component={props.component} />;
 };
 
-export default PrivateRoute;
+export {PrivateRoute, AccessRole};
