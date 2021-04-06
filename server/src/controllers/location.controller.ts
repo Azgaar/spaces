327caab914615304b87a9dce60045409cba51db6
Azgaar <maxganiev@yandex.ru>
2021-04-06 19:37:59 +0300

    server: error messages if no data from DB

--- server/src/controllers/location.controller.ts
+++ server/src/controllers/location.controller.ts
@@ -5,26 +5,32 @@ import locationService from "../services/location";
 
 const list = catchAsync(async (req, res, next) => {
   const locations = await locationService.list();
-  if (!locations) return next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Locations cannot be fetched"));
+  if (!locations) return next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Cannot fetch locations"));
 
   res.status(httpStatus.OK).send(locations);
 });
 
 const add = catchAsync(async (req, res, next) => {
   const {description} = req.body;
-  await locationService.add({description});
+  const location = await locationService.add({description});
+  if (!location) return next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Cannot add location"));
+
   list(req, res, next);
 });
 
 const rename = catchAsync(async (req, res, next) => {
   const {id, description} = req.body;
-  await locationService.rename({id, description});
+  const location = await locationService.rename({id, description});
+  if (!location) return next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Cannot rename location"));
+
   list(req, res, next);
 });
 
 const remove = catchAsync(async (req, res, next) => {
   const {id} = req.body;
-  await locationService.remove({id});
+  const location = await locationService.remove({id});
+  if (!location) return next(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Cannot remove location"));
+
   list(req, res, next);
 });
 
