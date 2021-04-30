aebed5191861334b8263752654d7608190e9f70e
Azgaar <maxganiev@yandex.ru>
2021-04-30 15:04:47 +0300

    server - Workspace search tests

--- server/tests/suites/workspaceManagement.test.ts
+++ server/tests/suites/workspaceManagement.test.ts
@@ -3,7 +3,7 @@ import httpStatus from "http-status";
 import App from "../../src/App";
 import config from "../../src/config";
 import {MongoMemory, extractCookies} from "../utils";
-import {UserData, UserRole, WorkspaceData, WorkspaceStatus, WorkspaceType, LocationData} from "../../src/types";
+import {UserData, UserRole, WorkspaceData, WorkspaceStatus, WorkspaceType, LocationData, Equipment} from "../../src/types";
 import {createUser} from "../../src/services/user";
 import locationService from "../../src/services/location";
 
@@ -13,7 +13,7 @@ let cookie = {admin: "", user: ""};
 const admin: UserData = {email: "admin@reg.com", firstName: "John", lastName: "Doe", password: "Secret12", role: UserRole.ADMIN};
 const user: UserData = {email: "user@reg.com", firstName: "Jane", lastName: "Dowson", password: "Secret1234", role: UserRole.USER};
 const location: LocationData = {description: "testLoc1"};
-const workspace: WorkspaceData = {description: "WS_1", location: "", status: WorkspaceStatus.AVAILABLE, type: WorkspaceType.DESK, size: 1, equipment: []};
+const workspace: WorkspaceData = {description: "WS_1", location: "", status: WorkspaceStatus.AVAILABLE, type: WorkspaceType.DESK, size: 1, equipment: [Equipment.HEADSET]};
 
 describe("Workspace management service", () => {
   beforeAll(async () => {
@@ -68,17 +68,16 @@ describe("Workspace management service", () => {
   });
 
   it("allows to update workspace for admin", async () => {
-    const description = "renameWorkspace";
-    const response = await request(app).post("/updateWorkspace").set("Cookie", cookie.admin).send({...workspace, description}).expect(httpStatus.OK);
+    const status = WorkspaceStatus.UNAVAILABLE;
+    const response = await request(app).post("/updateWorkspace").set("Cookie", cookie.admin).send({...workspace, status}).expect(httpStatus.OK);
     expect(Array.isArray(response.body)).toBe(true);
     expect(response.body.length).toBe(1);
-    expect(response.body[0].description).toBe(description);
-    expect(response.body[0].location).toBe(workspace.location);
-    expect(response.body[0].status).toBe(workspace.status);
-    expect(response.body[0].type).toBe(workspace.type);
-    expect(response.body[0].size).toBe(workspace.size);
-    expect(Array.isArray(response.body[0].equipment)).toBe(true);
-    expect(response.body[0].equipment.length).toBe(0);
+    expect(response.body[0].description).toBe(workspace.description);
+    expect(response.body[0].status).toBe(status);
+  });
+
+  it("fails to update unknown workspace", async () => {
+    await request(app).post("/updateWorkspace").set("Cookie", cookie.admin).send({...workspace, id: "606f1d6eb20d7e35e0360000"}).expect(httpStatus.INTERNAL_SERVER_ERROR);
   });
 
   it("fails to update workspace to incorrect status", async () => {
@@ -98,18 +97,18 @@ describe("Workspace management service", () => {
     expect(response.body.length).toBe(0);
   });
 
-  it("is not allowed for unauthorized user", async () => {
-    await request(app).post("/addWorkspace").send(workspace).expect(httpStatus.BAD_REQUEST);
-    await request(app).post("/getWorkspaces").send({location: workspace.location}).expect(httpStatus.BAD_REQUEST);
-    await request(app).post("/updateWorkspace").send(workspace).expect(httpStatus.BAD_REQUEST);
-    await request(app).delete("/deleteWorkspaces").send({location: workspace.location, selection: [workspace.id]}).expect(httpStatus.BAD_REQUEST);
-  });
-
   it("is not allowed for non-admin", async () => {
     await request(app).post("/addWorkspace").set("Cookie", cookie.user).send(workspace).expect(httpStatus.INTERNAL_SERVER_ERROR);
     await request(app).post("/getWorkspaces").set("Cookie", cookie.user).send({location: workspace.location}).expect(httpStatus.INTERNAL_SERVER_ERROR);
     await request(app).post("/updateWorkspace").set("Cookie", cookie.user).send(workspace).expect(httpStatus.INTERNAL_SERVER_ERROR);
     await request(app).delete("/deleteWorkspaces").set("Cookie", cookie.user).send({location: workspace.location, selection: [workspace.id]}).expect(httpStatus.INTERNAL_SERVER_ERROR);
+  });
+
+  it("is not allowed for unauthorized user", async () => {
+    await request(app).post("/addWorkspace").send(workspace).expect(httpStatus.BAD_REQUEST);
+    await request(app).post("/getWorkspaces").send({location: workspace.location}).expect(httpStatus.BAD_REQUEST);
+    await request(app).post("/updateWorkspace").send(workspace).expect(httpStatus.BAD_REQUEST);
+    await request(app).delete("/deleteWorkspaces").send({location: workspace.location, selection: [workspace.id]}).expect(httpStatus.BAD_REQUEST);
   });
 
   afterAll(() => {
