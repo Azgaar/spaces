c6d39d38bd1a89f636e0d59dcff068379be94fe0
Azgaar <maxganiev@yandex.ru>
2021-04-30 19:05:21 +0300

    client - move MenuButtons to its own file

--- client/src/components/Layout/components/Header/Header.tsx
+++ client/src/components/Layout/components/Header/Header.tsx
@@ -1,65 +1,14 @@
 import React, {useState} from "react";
 import useStyles from "./Header.style";
-import {AppBar, Toolbar, Box, Button, Drawer, List, MenuItem, Typography, useTheme, useMediaQuery} from "@material-ui/core";
-import {Link as RouterLink, useHistory} from "react-router-dom";
-import {useToasterCatcher, useUser} from "../../../../hooks";
-import {actions} from "../../../../store/actions";
-import {useDispatch} from "react-redux";
-import {AuthService} from "../../../../services";
+import {AppBar, Toolbar, Box, Button, Drawer, List,  Typography, useTheme, useMediaQuery} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuButtons from "./MenuButtons/MenuButtons";
 
 const logoPath = process.env.PUBLIC_URL + "/logo.svg";
-
-const UnauthorizedUserNavButtons = () => {
-  return (<>
-    <MenuItem color="inherit" component={RouterLink} to="/signin">Sign In</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/signup">Sign Up</MenuItem>
-  </>
-  );
-}
-
-const AuthorizedUserNavButtons = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const {catchAndTossError} = useToasterCatcher();
-
-  const handleLogout = async () => {
-    const result = await catchAndTossError(AuthService.logout())
-    if (!result) return
-    dispatch(actions.logout());
-    history.push("/signin");
-  }
-
-  return (<>
-    <MenuItem color="inherit" component={RouterLink} to="/profile">Profile</MenuItem>
-    <MenuItem color="inherit" onClick={handleLogout}>Log Out</MenuItem>
-  </>
-  );
-}
-
-const AdminNavButtons = () => {
-  return (<>
-    <MenuItem color="inherit" component={RouterLink} to="/admin/reservations">Reservations</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/admin/requests">Requests</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/admin/workspaces">Workspaces</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/admin/users">Users</MenuItem>
-  </>
-  );
-}
-
-const UserNavButtons = () => {
-  return (<>
-    <MenuItem color="inherit" component={RouterLink} to="/reserve">Reserve</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/reservations">Reservations</MenuItem>
-    <MenuItem color="inherit" component={RouterLink} to="/history">History</MenuItem>
-  </>
-  );
-}
 
 function Header() {
   const classes = useStyles();
   const wideScreen = useMediaQuery(useTheme().breakpoints.up("md"));
-  const {isAuthenticated, isAdmin} = useUser();
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleMenu = () => setMenuOpen(() => true);
@@ -72,24 +21,16 @@ function Header() {
         <Typography variant="h6" component="h1" className={classes.title}>SPɅCES</Typography>
 
         <Box display="flex">
-          {wideScreen ? (
-            <>
-              {isAuthenticated && (isAdmin ? <AdminNavButtons /> : <UserNavButtons />)}
-              {isAuthenticated ? <AuthorizedUserNavButtons /> : <UnauthorizedUserNavButtons />}
-            </>
-          ) : (
-            <>
-              <Button onClick={handleMenu} color="inherit" endIcon={<MenuIcon fontSize="large" />}>Menu</Button>
-              <Drawer open={menuOpen} onClose={handleClose} anchor="right" className={classes.drawer}>
-                <List>
-                  {isAuthenticated && (isAdmin ? <AdminNavButtons /> : <UserNavButtons />)}
-                  {isAuthenticated ? <AuthorizedUserNavButtons /> : <UnauthorizedUserNavButtons />}
-                </List>
-              </Drawer>
-            </>
-          )}
+          {wideScreen
+            ? <MenuButtons />
+            : <Button onClick={handleMenu} color="inherit" endIcon={<MenuIcon fontSize="large" />}>Menu</Button>
+          }
         </Box>
       </Toolbar>
+
+      <Drawer open={menuOpen} onClose={handleClose} anchor="right" className={classes.drawer}>
+        <List><MenuButtons /></List>
+      </Drawer>
     </AppBar>
   );
 }
---
+++ client/src/components/Layout/components/Header/MenuButtons/MenuButtons.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {MenuItem} from "@material-ui/core";
+import {Link as RouterLink, useHistory} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {AuthService} from "../../../../../services";
+import {actions} from "../../../../../store/actions";
+import {useToasterCatcher, useUser} from "../../../../../hooks";
+
+const UnauthorizedUserNavButtons = () => {
+  return (<>
+    <MenuItem color="inherit" component={RouterLink} to="/signin">Sign In</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/signup">Sign Up</MenuItem>
+  </>
+  );
+}
+
+const AuthorizedUserNavButtons = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const {catchAndTossError} = useToasterCatcher();
+
+  const handleLogout = async () => {
+    const result = await catchAndTossError(AuthService.logout())
+    if (!result) return
+    dispatch(actions.logout());
+    history.push("/signin");
+  }
+
+  return (<>
+    <MenuItem color="inherit" component={RouterLink} to="/profile">Profile</MenuItem>
+    <MenuItem color="inherit" onClick={handleLogout}>Log Out</MenuItem>
+  </>
+  );
+}
+
+const AdminNavButtons = () => {
+  return (<>
+    <MenuItem color="inherit" component={RouterLink} to="/admin/reservations">Reservations</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/admin/requests">Requests</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/admin/workspaces">Workspaces</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/admin/users">Users</MenuItem>
+  </>
+  );
+}
+
+const UserNavButtons = () => {
+  return (<>
+    <MenuItem color="inherit" component={RouterLink} to="/reserve">Reserve</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/reservations">Reservations</MenuItem>
+    <MenuItem color="inherit" component={RouterLink} to="/history">History</MenuItem>
+  </>
+  );
+}
+
+const MenuButtons = () => {
+  const {isAuthenticated, isAdmin} = useUser();
+  return (<>
+    {isAuthenticated && (isAdmin ? <AdminNavButtons /> : <UserNavButtons />)}
+    {isAuthenticated ? <AuthorizedUserNavButtons /> : <UnauthorizedUserNavButtons />}
+  </>)
+}
+
+export default MenuButtons;
