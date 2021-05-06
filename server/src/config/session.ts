83407c9fc155305e66cf39c1cba25597a874994d
Azgaar <maxganiev@yandex.ru>
2021-05-06 20:26:28 +0300

    also have to set domain

--- server/src/config/session.ts
+++ server/src/config/session.ts
@@ -8,19 +8,23 @@ type SessionConfig = {
     sameSite: boolean | 'none' | 'lax' | 'strict' | undefined;
     httpOnly: boolean;
     secure: boolean;
+    domain: string | undefined;
   };
 };
 
 export const getSessionConfig = (): SessionConfig => {
+  const PROD = process.env.NODE_ENV === 'production';
+
   const name = process.env.SESSION_NAME || 'sid';
   const resave = false;
   const saveUninitialized = false;
   const secret = process.env.SESSION_SECRET || '21F463B8C3489';
 
   const maxAge = Number(process.env.SESSION_LIFETIME) || 1000 * 60 * 60 * 2; // two hours
-  const sameSite = 'none';
+  const sameSite = PROD ? 'none' : true;
+  const domain = process.env.CLIENT ? new URL(process.env.CLIENT).hostname : '';
   const httpOnly = true;
-  const secure = process.env.NODE_ENV === 'production';
+  const secure = PROD;
 
-  return {name, resave, saveUninitialized, secret, cookie: {maxAge, sameSite, httpOnly, secure}};
+  return {name, resave, saveUninitialized, secret, cookie: {maxAge, sameSite, httpOnly, secure, domain}};
 };
