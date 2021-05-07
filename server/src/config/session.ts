d721d0be10b93dab2d2cc32976efb5221b800465
Azgaar <maxganiev@yandex.ru>
2021-05-07 15:54:21 +0300

    change session security settings, SSL allowed

--- server/src/config/session.ts
+++ server/src/config/session.ts
@@ -5,23 +5,24 @@ type SessionConfig = {
   secret: string;
   cookie: {
     maxAge: number;
-    sameSite: boolean;
+    sameSite: SameSite;
     httpOnly: boolean;
     secure: boolean;
     domain: string;
   };
 };
+type SameSite = boolean | 'lax' | 'strict' | 'none' | undefined;
 
 export const getSessionConfig = (): SessionConfig => {
   const name = process.env.SESSION_NAME || 'sid';
+  const secret = process.env.SESSION_SECRET || 'ver#$ijnwtrwFEF5%$eRkmgd';
   const resave = false;
   const saveUninitialized = false;
-  const secret = process.env.SESSION_SECRET || '21F463B8C3489';
 
-  const maxAge = Number(process.env.SESSION_LIFETIME) || 1000 * 60 * 60 * 2; // two hours
-  const sameSite = true;
-  const httpOnly = true;
-  const secure = process.env.NODE_ENV === 'production';
+  const maxAge = Number(process.env.SESSION_LIFETIME) || 1000 * 60 * 60 * 8; // 8 hours;
+  const sameSite = (process.env.COOKIE_SAME_SITE as SameSite) || true;
+  const httpOnly = Boolean(process.env.COOKIE_HTTP_ONLY) || true;
+  const secure = Boolean(process.env.COOKIE_SECURE) || false;
   const domain = String(process.env.CLIENT);
 
   return {name, resave, saveUninitialized, secret, cookie: {maxAge, sameSite, httpOnly, secure, domain}};
