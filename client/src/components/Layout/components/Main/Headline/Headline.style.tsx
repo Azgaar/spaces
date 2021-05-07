14a614b7c7b3708ed165f746b383812dbb181a0a
Azgaar <maxganiev@yandex.ru>
2021-05-07 18:05:34 +0300

    fix styles

--- client/src/components/Layout/components/Main/Headline/Headline.style.tsx
+++ client/src/components/Layout/components/Main/Headline/Headline.style.tsx
@@ -2,8 +2,7 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.primary.main
+    margin: theme.spacing(1)
   },
   header: {
     zIndex: 1
